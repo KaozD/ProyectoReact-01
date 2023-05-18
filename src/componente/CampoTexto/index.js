ca23badded707ec038b8c420bea7c035ea51ef0f
carlos <carlosest.aguilarr@gmail.com>
2023-05-17 23:30:36 -0600

    Pequeño avance en la clase 5

--- src/componente/CampoTexto/index.js
+++ src/componente/CampoTexto/index.js
@@ -1,12 +1,12 @@
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
-    console.log("Datos", props.titulo)
+    console.log("Datos", props)
 
     const placeholderMod = `${props.placeholder}...`;
     return <div className = "campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder = {placeholderMod}/>
+        <input placeholder = {placeholderMod} required ={props.required} />
 
     </div>
     
--- src/componente/Formulario/Formulario.js
+++ src/componente/Formulario/Formulario.js
@@ -14,9 +14,9 @@ const Formulario = () => {
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador</h2>
-            <CampoTexto titulo="Nombre" placeholder = "Ingresar el nombre" />
-            <CampoTexto titulo="Puesto" placeholder = "Ingresa el puesto" />
-            <CampoTexto titulo="Foto" placeholder = "Ingresar enlace de foto" />
+            <CampoTexto titulo="Nombre" placeholder = "Ingresar el nombre" required />
+            <CampoTexto titulo="Puesto" placeholder = "Ingresa el puesto" required />
+            <CampoTexto titulo="Foto" placeholder = "Ingresar enlace de foto" required />
             <ListaOpciones />
             <Boton>
                 Crear
