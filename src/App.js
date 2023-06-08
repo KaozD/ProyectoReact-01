eef96969c8217a59d090c58f3ca64a6acceb416d
carlos <carlosest.aguilarr@gmail.com>
2023-06-08 14:35:04 -0600

    Curso React y como funcionan los componentes C 1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Header from './componente/Header/header.js';
 import Formulario from './componente/Formulario/Formulario.js';
 import MiOrg from './componente/MiOrg';
+import Equipo from './componente/Equipo';
 
 
 function App() {
 
-  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
 
   //Ternario --> condicion ? seMuestra : noSeMuestra
   //condicion && seMuestra
@@ -27,6 +28,13 @@ function App() {
       { /*mostrarFormulario && <Formulario />*/ }
        
       <MiOrg cambiarMostrar = {cambiarMostrar}  />
+      <Equipo equipo="Programación" />
+      <Equipo equipo="Front End" />
+      <Equipo equipo="Data Science" />
+      <Equipo equipo="Dev Ops" />
+      <Equipo equipo="UX y Diseño" />
+      <Equipo equipo="Móvil" />
+      <Equipo equipo="Innovación y Gestión" />
       
     </div>
   );
